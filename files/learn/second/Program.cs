8ec7eed31c733d09fa4a9c82863da41e22869741
cofeek-codes <kormyshev11@mail.ru>
2022-03-04 10:08:00 +0500

    new

---
+++ files/learn/second/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "blue", "red", "green", "white", "black" };
+
+            FileStream col = new FileStream("colors.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            using (StreamWriter writer = new StreamWriter(col))
+            {
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    writer.WriteLine(colors[i]);
+                }
+            }
+
+
+        }
+    }
+}
